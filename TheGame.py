855054f3ca672b57be0d4ee3e7680e07b6fdc12d
Fizox <faris.ash03@gmail.com>
2022-06-01 00:43:56 +0300

    mew

---
+++ TheGame.py
@@ -0,0 +1,80 @@
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+#Just run the code don't take any look at it there is 5 ending
+from cat import cat
+
+
+n = 0
+vcat = cat()
+print("""hey I'm a cute vvu 
+I mean I'm just a cute cat ฅ^•ﻌ•^ฅ""")
+x = input("lets just play a little game just press 0 or 1 and you play with me (^◔ᴥ◔^) 🐾 don't put 10 or 01")
+
+if x == "0" or x == "1":
+    x = vcat.mew(x)
+    if x == "10" or x == "01":
+        x = vcat.now()
+if x == "0" or x == "1":
+    x = vcat.mow(x)
+    if x == "10" or x == "01":
+        print("I didn't knew it will hurt who programed me to say press 10 or 01")
+if x == "01" or x == "10":
+    x = vcat.now()
+    if x == "1" or x == "0":
+        vcat.bsbs()
+    elif x == "10" or x == "01":
+        vcat.bsbos(x)
+if x != "01" and x != "10" and x != "1" and x != "0":
+    vcat.new()
---
+++ cat.py
@@ -0,0 +1,133 @@
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+#open TheGame file don't cheat
+
+class cat:
+    def mew(self, x):
+        n = 0
+        while n != 2 and x != "10" and x != "01" and (x == "1" or x == "0"):
+            n += 1
+            x = input("i like that do it again 🐾 /ᐠ=✪ ᆺ ✪=ᐟ\ 🐾")
+            return x
+    def mow(self, x):
+        n = 0
+        while x == "1" or x == "0":
+            n += 1
+            if n < 5:
+                x = input("keep doing it ya don't press 01 or 10 (=ʘᆽʘ=)")
+                if x != "1" and x != "0":
+                    break
+            if n == 5:
+                x = input("are you stupid press 01 or 10 to complete the game(^._.^)⟆")
+                if x != "1" and x != "0":
+                    break
+            if n == 6:
+                x = input("congratulations you got a degree in not putting 01 or 10 can you put it now (^•~•^) ?")
+                if x != "1" and x != "0":
+                    break
+            if n == 7:
+                x = input("i get it you don't want to beat me lets be friends i think (≽ܫ≼) ?")
+                if x != "1" and x != "0":
+                    break
+            if n == 8:
+                x = input("you unlock new achievement >>>being a friend<<< ₍^ >ヮ<^₎")
+                if x != "1" and x != "0":
+                    break
+            if n == 9:
+                print("you have been fooled you are not my friend I'm out no body want to be your friend ｡＾･ｪ･＾｡")
+                break
+        return x
+    def now(self):
+        print("I said don't put it are you stupid I lost one of my lives don't do it again ^.--.^")
+        x = input("just please don't put it again 0 or 1 plz (=ꆤᆽꆤ=)")
+        return x
+    def new(self):
+        print('congratulations you are too stupid for me as a cat virus to hack you wish you good luck bey =＾● ⋏ ●＾= ')
+
+    def bsbs(self):
+        print("Thx for saving my life I'm sorry for the damage I did now because I'm a virus I'm going to suicide "
+              "wish you luck /ᐠ .ᆺ. ᐟ\ﾉ")
+
+    def bsbos(self , x):
+        n = 0
+        while (True):
+            n+=1
+            if n == 1 :
+                x = input("now i have only 5 lives stooop plz ि०॰͡०ी")
+                if x == "0" or x == "1":
+                    break
+            if n == 2 :
+                x = input(" just stop relax plz Im sorry/ᐠơ ﻌ ơᐟ\ ")
+                if x == "0" or x == "1":
+                    break
+            if n == 3 :
+                x = input("it's not funny 3 lives left₍⸍⸌̣ʷ̣̫⸍̣⸌₎")
+                if x == "0" or x == "1":
+                    break
+            if n == 4 :
+                x = input("kill me I'm done cat with two lives is that a joke-ᄒᴥᄒ-")
+                if x == "0" or x == "1":
+                    break
+            if n == 5 :
+                x = input("I'm kidding don't kill me i got kids plz (=ꆤᆽꆤ=)")
+                if x == "0" or x == "1":
+                    break
+            if n == 6 :
+                print("""I'm a virus and good actor so im not dead
+so you are so stupid trying to kill an innocent cat
+I will always watch you sick person ｡＾･ｪ･＾｡""")
+                return x
+                break
+        if x == "1" or x == "0":
+            print("I'm out of this filthy computer bey ि०॰͡०ी")
+        else:
+            print('congratulations you are too stupid for me as a cat virus to hack you wish you good luck bey =＾● ⋏ ●＾= ')
